73af743097c8c99ca4d352b9194f734237c5a86e
Ziadkhaled8 <Ziadk6872@gmail.com>
2024-05-19 19:45:19 +0300

    Error messages intialized

--- Wajbah_API/Models/APIResponse.cs
+++ Wajbah_API/Models/APIResponse.cs
@@ -6,7 +6,7 @@ namespace Wajbah_API.Models
     {
         public  HttpStatusCode StatusCode { get; set; }
         public bool IsSuccess { get; set; } = true;
-        public List<string> ErrorMessages { get; set; }
+        public List<string> ErrorMessages { get; set; }=new List<string>() { };
         public object Result { get; set; }
     }
 }
