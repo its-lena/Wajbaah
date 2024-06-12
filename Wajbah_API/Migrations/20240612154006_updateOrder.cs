9f5d11cf2c3512678153994903d0ccfcfd01597c
its-lena <lenagamal30@gmail.com>
2024-06-12 18:41:26 +0300

    Order table updated

---
+++ Wajbah_API/Migrations/20240612154006_updateOrder.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Wajbah_API.Migrations
+{
+    /// <inheritdoc />
+    public partial class updateOrder : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CustomerPhoneNumber",
+                table: "Orders");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "CustomerPhoneNumber",
+                table: "Orders",
+                type: "int",
+                nullable: true);
+        }
+    }
+}
